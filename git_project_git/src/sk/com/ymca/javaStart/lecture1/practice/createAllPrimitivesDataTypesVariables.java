77372cce5b53d2a1914a7ff2ec273c8b2206cc9e
maksim melnychenko <melnychenko.maks@gmail.com>
2024-03-23 15:40:15 +0100

    class work

---
+++ git_project_git/src/sk/com/ymca/javaStart/lecture1/practice/createAllPrimitivesDataTypesVariables.java
@@ -0,0 +1,17 @@
+package sk.com.ymca.javaStart.lecture1.practice;
+
+public class createAllPrimitivesDataTypesVariables {
+    public static void main(String[] args) {
+        byte firstVariable = 127;
+        short secondVariable = 3200;
+        int thirdVariable = 37896;
+        long fourthVariable = 1000000;
+        char fifthVariable = 'e';
+        float sixthVariable = 2.5F;
+        double seventhVariable = 40.65;
+        boolean eighthVariable = false;
+
+        System.out.println(firstVariable + "\n" + secondVariable + "\n" + thirdVariable + "\n" + fourthVariable + "\n"
+                + fifthVariable + "\n" + sixthVariable + "\n" + seventhVariable + "\n" + eighthVariable);
+    }
+}
---
+++ git_project_git/src/sk/com/ymca/javaStart/lecture2/practice/Questionnaire.java
@@ -0,0 +1,15 @@
+package sk.com.ymca.javaStart.lecture2.practice;
+
+public class Questionnaire {
+    public static void main(String[] args) {
+        String name = "Maksim";
+        String surname = "Melnychenko";
+        char index = '@';
+        final int DATEOFBIRTH = 24102007;
+        double yearsOld = 16.4;
+        boolean checkForRobot = false;
+
+        System.out.println("Name = " + name + ", surname = " + surname + ", index = " + index + ", date of birth = "
+        + DATEOFBIRTH + ", Years old = " + yearsOld + ", check for robot = " + checkForRobot);
+    }
+}
